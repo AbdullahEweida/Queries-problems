89a89f1faeb99acd19d174aa777548d9b7922d45
ِAbdullahEweida <https://github.com/AbdullahEweida>
2025-11-22 12:14:58 +0200

    View manager name and employee

---
+++ Problem #51.sql
@@ -0,0 +1,3 @@
+--View manager name and employee
+SELECT Employees.Name, Employees.ManagerID, Employees.Salary, Managers.Name AS ManagerName
+FROM Employees INNER JOIN Employees AS Managers ON Employees.ManagerID = Managers.EmployeeID
