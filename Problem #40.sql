9c67e375b1b1ee2a8d5190b34915d677acde8ee5
ِAbdullahEweida <https://github.com/AbdullahEweida>
2025-11-22 12:05:54 +0200

    Get Total Number Of Doors Manufactured by 'Ford'

---
+++ Problem #40.sql
@@ -0,0 +1,4 @@
+--Get all vehicles that has one of the Max 3 Engine CC
+SELECT * FROM VehicleDetails
+WHERE Engine_CC IN (SELECT DISTINCT TOP 3 Engine_CC FROM VehicleDetails 
+                    ORDER BY Engine_CC DESC )
